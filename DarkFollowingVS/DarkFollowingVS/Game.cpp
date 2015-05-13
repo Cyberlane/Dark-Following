262cd7a6a92f98a6a8755fa10266b27a31c33bce
Ivy Fae <ivy@nationalcorp.com>
2015-05-13 15:20:23 -0500

    Added basic stuff to refresh myself

---
+++ DarkFollowingVS/DarkFollowingVS/Game.cpp
@@ -0,0 +1,31 @@
+#include "Game.h"
+
+
+Game::Game()
+{
+}
+
+
+Game::~Game()
+{
+}
+
+void Game::Start()
+{
+	return;
+}
+
+void Game::Pause()
+{
+	return;
+}
+
+void Game::Stop()
+{
+	return;
+}
+
+char* Game::GetName()
+{
+	return "Dark Following";
+}
---
+++ DarkFollowingVS/DarkFollowingVS/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+#include "Game.h"
+
+int main(int argc, char**argv)
+{
+	Game* game = new Game();
+	cout << "Hello! The game "<< game->GetName() <<" has been instantiated." << endl;
+
+	getchar();
+	return 0;
+}
